abd29b8853c8cf92810e0b87850e34910db2c9d3
nikitamorozov <nikitwww@gmail.com>
2024-07-06 20:45:43 +0300

    ci(workflow): add python-package.yml

--- startup_scripts/unix.sh
+++ startup_scripts/unix.sh
@@ -16,10 +16,9 @@ done
 SCRIPT_PATH="$(readlink -f "${SCRIPT_PATH}")"
 SCRIPT_DIR="$(cd -P "$(dirname -- "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
 
-python3.11 -m venv venv
-source venv/bin/activate
+python3.11 -m venv .venv
+source .venv/bin/activate
 pip install --upgrade pip
 pip install python-telegram-bot==21.0.1
-pip install setproctitle
 cd ..
 screen -S 'alcounting_bot' python -m bot.__main__
